04ca044f3ba93483b4de11ae513d26db64c5a10d
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-09-17 23:17:37 +0300

    refactoring

--- main.js
+++ main.js
@@ -6,27 +6,41 @@ window.addEventListener("load", () => {
   const tasksListElement = document.getElementById("tasksList");
   const footer = document.getElementById("pendingTasks");
   const search = document.getElementById("search");
+  const storage = localStorage.getItem("tasks");
+  let todolist_state = false;
 
-  let tasksArr = localStorage.getItem("tasks")
-    ? JSON.parse(localStorage.getItem("tasks"))
-    : [];
+  let tasksArr = storage ? JSON.parse(storage) : [];
 
-  renderTasks();
+  renderTasks(tasksArr);
 
-  function deleteTask(index) {
+  function create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
+    return uuid;
+  }
+
+  const cancelBtn = document.getElementById("cancelDeleting");
+  const confDeleteBtn = document.getElementById("confirmDeleting");
+
+  function deleteTask(id) {
     showConfirmBox();
-
-    const cancelBtn = document.getElementById("cancelDeleting");
-    const confDeleteBtn = document.getElementById("confirmDeleting");
-
     cancelBtn.addEventListener("click", closeConfirmBox);
-
-    confDeleteBtn.addEventListener("click", () => {
+    confDeleteBtn.addEventListener("click", fun);
+    function fun() {
+      confDeleteBtn.removeEventListener("click", fun);
+      const index = tasksArr.findIndex((obj) => obj.id === id);
       tasksArr.splice(index, 1);
       localStorage.setItem("tasks", JSON.stringify(tasksArr));
-      renderTasks();
+      changeJustHappened(tasksArr);
       closeConfirmBox();
-    });
+    }
   }
 
   function showConfirmBox() {
@@ -37,78 +51,106 @@ window.addEventListener("load", () => {
     document.getElementById("overlay").hidden = true;
   }
 
-  function addTask() {
-    tasksArr.push({ task: inputTask.value, assignee: assignee.value });
-    localStorage.setItem("tasks", JSON.stringify(tasksArr));
+  function resetInputs() {
     inputTask.value = "";
     assignee.value = "";
-
     inputTask.focus();
-    renderTasks();
   }
 
-  function renderTasks() {
+  function addTask() {
+    tasksArr.push({
+      task: inputTask.value,
+      assignee: assignee.value,
+      id: create_UUID(),
+      isDone: false,
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasksArr));
+    resetInputs();
+    changeJustHappened(tasksArr);
+  }
+
+  function toggleTask(id) {
+    tasksArr = tasksArr.map((todo) =>
+      todo.id === id
+        ? {
+            task: todo.task,
+            assignee: todo.assignee,
+            id: todo.id,
+            isDone: !todo.isDone,
+          }
+        : todo
+    );
+    changeJustHappened(tasksArr);
+  }
+
+  function changeJustHappened(todos) {
+    todolist_state = true;
+    renderTasks(todos);
+  }
+
+  function renderTasks(todos, isSearch) {
+    console.log(todolist_state);
     tasksListElement.innerHTML = "";
-    tasksArr.forEach((element, index) => {
+    let res = todos.map((element) => {
       const li = document.createElement("li");
       li.className = "tasksList-element";
-      li.innerHTML = `
-        <div class="task">${element.task} <br> ${element.assignee}</div>
-        <div class="buttons">
-          <button class="delete";>Delete</button>
-          <button class="done";>Done</button>
-        </div>
-      `;
-
-      tasksListElement.append(li);
-
-      const deleteBtn = document.getElementsByClassName("delete")[index];
-      deleteBtn.addEventListener("click", () => deleteTask(index));
-
-      const doneBtn = document.getElementsByClassName("done")[index];
-      doneBtn.addEventListener("click", () => {
-        li.childNodes[1].classList.toggle("doneTask");
-      });
+      if (!element.isDone) {
+        li.innerHTML = `
+          <div class="task">${element.task} <br> ${element.assignee}</div>
+          <div class="buttons" id="${element.id}" >
+            <button class="delete" >Delete</button>
+            <button class="done";>Done</button>
+          </div>
+        `;
+      } else {
+        li.innerHTML = `
+          <div class="task"><s>${element.task} <br> ${element.assignee}</s></div>
+          <div class="buttons" id="${element.id}" >
+            <button class="delete" >Delete</button>
+            <button class="done";>Done</button>
+          </div>
+        `;
+      }
+      return li;
     });
-    footer.innerHTML = `You have ${tasksArr.length} pending tasks. `;
+
+    tasksListElement.append(...res);
+
+    let task_div = tasksListElement.getElementsByClassName("buttons");
+    for (let el of task_div) {
+      const task_btns = el.children;
+      const deleteBtn = task_btns[0];
+      deleteBtn.addEventListener("click", () => {
+        deleteTask(el.id);
+      });
+
+      const doneBtn = task_btns[1];
+      doneBtn.addEventListener("click", () => {
+        toggleTask(el.id);
+      });
+    }
+    if (isSearch) footer.innerHTML = `${res.length} search result found. `;
+    else footer.innerHTML = `You have ${todos.length} pending tasks. `;
   }
 
   btn.addEventListener("click", addTask);
 
   search.addEventListener("keydown", (event) => {
-    if (event.code == "Enter") {
-      let searchFor = search.value.toString().toLowerCase();
-      let res = tasksArr.filter(
-        (obj) =>
-          obj.task.toString().toLowerCase().includes(searchFor) ||
-          obj.assignee.toString().toLowerCase().includes(searchFor)
-      );
+    if (event.code !== "Enter") return;
 
-      tasksListElement.innerHTML = "";
-      res.forEach((element, index) => {
-        const li = document.createElement("li");
-        li.className = "tasksList-element";
-        li.innerHTML = `
-        <div class="task">${element.task} <br> ${element.assignee}</div>
-        <div class="buttons">
-          <button class="delete";>Delete</button>
-          <button class="done";>Done</button>
-        </div>
-      `;
-
-        tasksListElement.append(li);
-
-        const deleteBtn = document.getElementsByClassName("delete")[index];
-        deleteBtn.addEventListener("click", deleteTask);
-      });
-      footer.innerHTML = `${res.length} search result found. `;
-    }
+    const searchFor = search.value.toString().toLowerCase();
+    const res = tasksArr.filter(
+      (obj) =>
+        obj.task.toString().toLowerCase().includes(searchFor) ||
+        obj.assignee.toString().toLowerCase().includes(searchFor)
+    );
+    renderTasks(res, true);
   });
 
-  search.addEventListener("keyup", (event) => {
-    let searchFor = search.value.toString().toLowerCase();
+  search.addEventListener("keyup", () => {
+    const searchFor = search.value.toString().toLowerCase();
     if (!searchFor) {
-      renderTasks();
+      renderTasks(tasksArr);
     }
   });
 });
